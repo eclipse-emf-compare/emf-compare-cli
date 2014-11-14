357230401e44d3b6de082b88600d7f59f55e475b
Arthur Daussy <arthur.daussy@obeo.fr>
2014-11-14 10:46:57 +0100

    Improves the configuration script.
    This script can now be launched from any OS.
    
    Change-Id: I7014e91589f1a3c319c205be2fe8428b704ec76f

--- packaging/org.eclipse.emf.compare.git.pgm.scripts/tests/manTestConfiguration.sh
+++ packaging/org.eclipse.emf.compare.git.pgm.scripts/tests/manTestConfiguration.sh
@@ -10,13 +10,38 @@
 #    Obeo - initial API and implementation
 # ====================================================================
 
+# Running source manTestConfiguation.sh will configure your testing environment.
+# It will:
+#	Exports the EMF_COMPARE_GIT_PGM_PATH variable
+#	Adds EMF Compare CLI scripts to the path
 
 #Location of the current script
 scriptLocation="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "Exporting application varaible"
-# If the test in run on a linux gtk x86_64
-# TODO improve this
-export EMF_COMPARE_GIT_PGM_PATH="$scriptLocation/../../org.eclipse.emf.compare.git.pgm.product/target/products/org.eclipse.emf.compare.git.pgm.product/linux/gtk/x86_64/emfcompare-git-pgm"
+OS=""
+FILE_EXT=""
+if [[ "${OSTYPE}" == "linux"* || "${OSTYPE}" == "freebsd"* ]]; then
+	OS="linux/gtk"
+elif [[ "${OSTYPE}" == "cygwin"* ]]; then
+	OS="win32"
+	FILE_EXT=".exe"
+elif [[ "${OSTYPE}" == "darwin"* ]]; then
+	OS="macosx"
+else
+	LSCRITICAL "Unknown 'OSTYPE'=${OSTYPE}."
+	exit -1
+fi
 
+if [[ $(uname -m) == *"64"* ]]; then
+	ARCH="x86_64"
+else
+	ARCH="x86"
+fi
+
+PLATFORM_SPECIFIER="${OSWS}.${ARCH}"
+
+echo "Exporting application variables EMF_COMPARE_GIT_PGM_PATH"
+export EMF_COMPARE_GIT_PGM_PATH="$scriptLocation/../../org.eclipse.emf.compare.git.pgm.product/target/products/org.eclipse.emf.compare.git.pgm.product/${OS}/${ARCH}/emfcompare-git-pgm${FILE_EXT}"
+
+echo "Adding EMF Compare CLI scripts to the PATH"
 # Adds the location of the folder holding git scripts to the path. This way the command "git SCRIPT_NAME ARGS" will work.
-PATH=$scriptLocation/..:$PATH
+PATH=$scriptLocation/../scripts:$PATH
